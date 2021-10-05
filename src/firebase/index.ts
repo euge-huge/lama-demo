1cf251073c95acfd04fadcc2d70425373da76b67
Eugene Bystrov <evgeniy0ctreles@gmail.com>
2021-10-05 12:02:07 +0700

    #

--- src/firebase/index.ts
+++ src/firebase/index.ts
@@ -2,8 +2,8 @@ import { initializeApp } from 'firebase/app'
 
 export const app = initializeApp({
   apiKey: 'AIzaSyAu41s4ST0yZHYOBcsFbRxcTyfbojWJx8k',
-  authDomain: 'lama-demo.firebaseapp.com',
   projectId: 'lama-demo',
+  authDomain: 'lama-demo.firebaseapp.com',
   storageBucket: 'lama-demo.appspot.com',
   messagingSenderId: '888524152302',
   appId: '1:888524152302:web:e7fd37cb01d82bd2b042ff',
